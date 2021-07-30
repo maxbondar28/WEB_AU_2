682eda6547ab67e571bb6ca16937cc408720064b
maxbondar28 <Maksym.Bondarenko@bitmedia.com.ua>
2021-07-30 15:36:45 +0300

    Initial commit

---
+++ src/test/java/Main.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+public class Main {
+
+    String SITE_URL = "";
+    WebDriver driver;
+
+    @Test
+    public void firstTest(){
+
+
+    };
+
+    @Test
+    public void secondTest(){
+
+
+    };
+}
